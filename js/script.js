a488c8c2ceffcc7799417fdf602020ec24c0dc49
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-17 14:50:06 +0300

    adding function to js to output random number

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("button#roll").click(function(){
+    });
+});
+function randomNumber(){
+       var min = 1;
+       var max = 6;
+       var result = Math.floor(Math.random()*max)+min;
+       // alert (result); function that tests random number
+};
