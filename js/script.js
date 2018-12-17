beaf5aeb4401ffda318ab96528bd1da39685e5a5
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-17 19:43:20 +0300

    testing the buttons using alert message

--- js/script.js
+++ js/script.js
@@ -2,9 +2,17 @@ $(document).ready(function(){
   $("button#roll").click(function(){
     });
 });
+// var i= 1;
+// for(i=1; i<=6; i++);
 function randomNumber(){
        var min = 1;
        var max = 6;
        var result = Math.floor(Math.random()*max)+min;
-       // alert (result); function that tests random number
+      //alert (result);  // function that tests random number
+if (result > 1){
+alert("continue")
+}
+else{
+alert("stop!")
+}
 };
