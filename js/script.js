77a0a878fc70a0e74635b4144c365bb7e102c740
Nyagajrr <muchokijnrbenson@gmail.com>
2018-12-18 13:58:32 +0300

    random number

--- js/script.js
+++ js/script.js
@@ -1,22 +1,52 @@
-// front end
+// front end jQuery
 $(document).ready(function(){
 
    $(".button-0").click(function(event){
      alert("alright!");
    });
-   $(".button-1").click(function(event){
+
+   $(".button-1").click(function(){
      //alert("hello world!");
-     alert(randomNumber());
+     randomNumber1();
+
      });
 
-    $(".button-2").click(function(event){
+    $(".button-2").click(function(){
        alert("check!");
     });
+
 });
 
-// back end
-
- function randomNumber(){
+// back end javascript
+ function randomNumber1(){
         var min = 1;
         var max = 6;
         var result = Math.floor(Math.random()*max)+min;
+        //return result;
+
+ if (result === 1){
+   alert("Its player 2's turn");
+   $('.box1').show();
+   $('.box2').hide();
+randomNumber2();
+ }else{
+   alert(result);
+ }
+};
+
+function randomNumber2(){
+       var min = 1;
+       var max = 6;
+       var result = Math.floor(Math.random()*max)+min;
+       //return result;
+
+if (result === 1){
+  alert("Its player 1's turn");
+  $('.box1').hide();
+  $('.box2').show();
+
+}
+randomNumber2();
+else{
+  alert(result);
+}};
